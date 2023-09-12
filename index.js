c28e2e8740755a408cc87c06f32e1fc586023846
Nikita <terencekity@gmail.com>
2023-09-12 10:10:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(arg){
+    if(!arg)return 'This Saturday, I want to roller-skate!'
+    else return 'This Saturday, I want to bathe my dog!'
+}
+function mondayWork(arg){
+if (!arg)return `This Monday, I will go to the office.`
+else return 'This Monday, I will work from home.'
+
+}
+function wrapAdjective(arg){
+   if (arg==='*')return function (adj){return `You are *${adj}*!`}
+   else if (arg==='||') return function (adj){return `You are ||${adj}||!`}
+}
